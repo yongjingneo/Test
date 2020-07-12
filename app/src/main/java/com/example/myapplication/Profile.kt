06b087e72753bf862d51397b81f16715ef57187a
yongjingneo <yj012999@gmail.com>
2020-07-12 18:25:43 +0800

    21

--- app/src/main/java/com/example/myapplication/Profile.kt
+++ app/src/main/java/com/example/myapplication/Profile.kt
@@ -1,25 +1,20 @@
 package com.example.myapplication
 
-import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.EditText
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class Profile : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
-
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        val actionbar = supportActionBar
+        actionbar!!.title="Profile"
 
         auth = FirebaseAuth.getInstance()
         displayProfile()
@@ -28,7 +23,7 @@ class Profile : AppCompatActivity() {
             startActivity(Intent(this, ChangePassword::class.java))
         }
 
-        btnCheck.setOnClickListener {
+        btnCheckRoom.setOnClickListener {
             startActivity(Intent(this, Reservation::class.java))
         }
     }
@@ -37,47 +32,10 @@ class Profile : AppCompatActivity() {
         val user = auth.currentUser
         if(user != null){
 
-//            if(user.displayName == null){
-//                val builder = AlertDialog.Builder(this)
-//                builder.setTitle("Set Username")
-//
-//                val view = layoutInflater.inflate(R.layout.dialog_set_name, null)
-//                val name = view.findViewById<EditText>(R.id.txtUsername)
-//                builder.setView(view)
-//                builder.setPositiveButton("Set", DialogInterface.OnClickListener { _, _ ->
-//                    setUsername(name)
-//                })
-//                builder.show()
-//            }
-
             txtEmail.setText(user.email)
             //updateProfile()
 
         }
     }
 
-//    private fun updateProfile(){
-//        val user = auth.currentUser
-//        if(user != null){
-//            user?.let {
-//                val name = user.displayName
-//                val email = user.email
-//
-//                txtName.setText(name.toString())
-//                txtEmail.setText(email.toString())
-//            }
-//        }
-//    }
-
-//    private fun setUsername(name: EditText){
-//        val user = auth.currentUser
-//
-//        val profileUpdates = UserProfileChangeRequest.Builder().setDisplayName(name.toString()).build()
-//
-//        user!!.updateProfile(profileUpdates).addOnCompleteListener { task ->
-//            if(task.isSuccessful){
-//                Toast.makeText(this, "Username Updated.", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//    }
 }
--- app/src/main/java/com/example/myapplication/Reservation.kt
+++ app/src/main/java/com/example/myapplication/Reservation.kt
@@ -2,11 +2,43 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.roomReservation.roomAdapter
+import com.example.myapplication.roomReservation.roomReservation
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_reservation.*
 
 class Reservation : AppCompatActivity() {
+
+    lateinit var ref: DatabaseReference
+    lateinit var roomList:MutableList<roomReservation>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservation)
+        val actionbar = supportActionBar
+        actionbar!!.title="Reservation"
+
+        ref = FirebaseDatabase.getInstance().getReference("roomReservations")
+        roomList = mutableListOf()
+
+        ref.addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if(p0.exists()){
+                    roomList.clear()
+                    for(r in p0.children){
+                        val room = r.getValue(roomReservation::class.java)
+                        roomList.add(room!!)
+                    }
+
+                    val adapter = roomAdapter(applicationContext, R.layout.rooms, roomList)
+                    listRoom.adapter = adapter
+                }
+            }
+
+        } )
     }
 }
