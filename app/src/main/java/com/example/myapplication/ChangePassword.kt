4277e79c3e149809b5174c13a93dfcfd019bb8d0
yongjingneo <yj012999@gmail.com>
2020-07-12 16:48:40 +0800

    19

--- app/src/main/java/com/example/myapplication/ChangePassword.kt
+++ app/src/main/java/com/example/myapplication/ChangePassword.kt
@@ -8,7 +8,6 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_change_password.*
